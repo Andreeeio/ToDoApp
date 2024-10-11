e7142e96292b5fe9d89731cf238de2c7f6bc5225
Andreeeio <164790744+Andreeeio@users.noreply.github.com>
2024-10-11 18:03:12 +0200

    First Configuration done

---
+++ ToDoApp.Infrastracture/Presistance/UserConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Infrastracture.Presistance;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasMany(a => a.Assignments)
+            .WithOne(u => u.User)
+            .HasForeignKey(i => i.User_Id)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasIndex(e => e.Email)
+            .IsUnique();
+
+        builder.HasIndex(p => p.Phone)
+            .IsUnique();
+
+    }
+}
