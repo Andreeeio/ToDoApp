d847ebb5d0667a8381d36131438d281dae742633
Andreeeio <164790744+Andreeeio@users.noreply.github.com>
2024-10-18 12:29:22 +0200

    Register user

--- ToDoApp.Api/Controllers/UserControler.cs
+++ ToDoApp.Api/Controllers/UserControler.cs
@@ -15,31 +15,34 @@ public class UserControler(IMediator mediator) : ControllerBase
 {
     private readonly IMediator _mediator = mediator;
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<IActionResult> GetAll()
     {
         var user = await _mediator.Send(new GetAllUsersQuery());
         return Ok(user);
     }
 
-    [HttpGet("{id}")]   
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetById([FromRoute] int id)
     {
         var user = await _mediator.Send(new GetUserByIdQuery(id));
-        if (user == null)
-            return BadRequest();
         return Ok(user);
     }
 
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> CreateUser([FromBody] AddUserCommand command)
     {
-        if(await _mediator.Send(command) == 0)
-        {
-            return BadRequest(); 
-        }
+        await _mediator.Send(command);
         return Created();
     }
     [HttpPatch("confirmemail")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> ConfirmEmail(ConfirmEmailCommand command)
     {
         await _mediator.Send(new ConfirmEmailCommand(command.Token, command.Email));
