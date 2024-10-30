fbcaa9dfef32c23c17cfc1ceeed525d4bbf57b42
Andreeeio <164790744+Andreeeio@users.noreply.github.com>
2024-10-30 15:46:14 +0100

    Backend implemenation

---
+++ ToDoApp.Infrastracture/Extentions/ServiceCollectionExtention.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToDoApp.Infrastracture.Presistance;
+
+namespace ToDoApp.Infrastracture.Extentions;
+
+public static class ServiceCollectionExtention
+{
+    public static void AddInfrastracture(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("ToDoApp");
+        services.AddDbContext<ToDoAppDbContext>(options => options.UseSqlServer(connectionString));
+    }
+}
---
+++ ToDoApp.Infrastracture/Presistance/AssignmentConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Infrastracture.Presistance;
+
+public class AssignmentConfiguration : IEntityTypeConfiguration<Assignment>
+{
+    public void Configure(EntityTypeBuilder<Assignment> builder)
+    {
+        builder.HasOne(u => u.User)
+            .WithMany(u => u.Assignments)
+            .HasForeignKey(i => i.User_Id)
+            .OnDelete(DeleteBehavior.Cascade);
+
+    }
+}
